d127e4c5c43383c1ae0a0161e8282b8edfca60ee
popenko-ikrok <popenko.d@ikrok.net>
2023-10-31 10:22:38 +0000

    fix: Add translation

--- erpnext/templates/pages/cart.py
+++ erpnext/templates/pages/cart.py
@@ -4,8 +4,11 @@
 no_cache = 1
 
 from erpnext.e_commerce.shopping_cart.cart import get_cart_quotation
-
+from frappe.translate import get_all_translations
+import frappe
 
 def get_context(context):
 	context.body_class = "product-page"
 	context.update(get_cart_quotation())
+	language = get_all_translations(frappe.local.lang)
+	context.translated_messages = language
